db977c1d554d8d50f874b8aacb03da4e0a6c75d7
Vladyslav Hurskyi <vladyslav@mgroupweb.com>
2023-11-14 11:03:05 +0200

    [add] (liquid): footer, footer bar; added condition for all blocks

---
+++ assets/footer-bottom-bar.js
@@ -0,0 +1,7 @@
+const selector = document.querySelector(".main-footer__bottom-language")
+
+  function addClassOnClick() {
+    selector.classList.toggle('active');
+  }
+
+selector.addEventListener('click', addClassOnClick);
